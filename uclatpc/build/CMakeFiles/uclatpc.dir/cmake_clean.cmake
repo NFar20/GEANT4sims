e597543d0fb7080971ea4c742cdbd33b8cc7594a
NFar20 <97466047+NFar20@users.noreply.github.com>
2025-07-01 15:42:31 -0700

    Add uclatpc project
    same code as sim project, just in uclatpc folder so I can edit using vs code

---
+++ uclatpc/build/CMakeFiles/uclatpc.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uclatpc.dir/link.d"
+  "CMakeFiles/uclatpc.dir/src/action.cc.o"
+  "CMakeFiles/uclatpc.dir/src/action.cc.o.d"
+  "CMakeFiles/uclatpc.dir/src/construction.cc.o"
+  "CMakeFiles/uclatpc.dir/src/construction.cc.o.d"
+  "CMakeFiles/uclatpc.dir/src/detector.cc.o"
+  "CMakeFiles/uclatpc.dir/src/detector.cc.o.d"
+  "CMakeFiles/uclatpc.dir/src/generator.cc.o"
+  "CMakeFiles/uclatpc.dir/src/generator.cc.o.d"
+  "CMakeFiles/uclatpc.dir/src/physics.cc.o"
+  "CMakeFiles/uclatpc.dir/src/physics.cc.o.d"
+  "CMakeFiles/uclatpc.dir/src/run.cc.o"
+  "CMakeFiles/uclatpc.dir/src/run.cc.o.d"
+  "CMakeFiles/uclatpc.dir/uclatpc.cc.o"
+  "CMakeFiles/uclatpc.dir/uclatpc.cc.o.d"
+  "uclatpc"
+  "uclatpc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uclatpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
